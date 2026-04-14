3b1922613e42f34d7a9e31f2d0d77c10ab3a8cb1
Hillel Coren <hillelcoren@gmail.com>
2026-04-14 12:42:48 +0300

    Task overview shows false costs #747

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -588,7 +588,7 @@ abstract class TaskEntity extends Object
   }
 
   double calculateAmount(double taskRate) =>
-      taskRate * round(calculateDuration().inSeconds / 3600, 3);
+      taskRate * round(calculateDuration(onlyBillable: true).inSeconds / 3600, 3);
 
   Duration calculateDuration({bool onlyBillable = false}) {
     int seconds = 0;
